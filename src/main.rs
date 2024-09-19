78b884821bd63177f776bd80046fcc6f24a913a1
Sam Hill <s.hill@yorksj.ac.uk>
2024-09-19 14:10:04 +0100

    Add in lsm303agr with embassy

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,32 @@
 #![no_main]
 
 mod button;
+mod led;
+mod sensors;
 
 use button::ButtonDirection;
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{AnyPin, Input, Level, Output, OutputDrive, Pin, Pull};
+use embassy_nrf::{self as hal, gpio::{AnyPin, Input, Level, Output, OutputDrive, Pin, Pull}, twim::Twim};
 use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, channel::Channel};
 use embassy_time::Timer;
+use embassy_time::Delay;
+use futures::{select_biased, FutureExt};
+use hal::twim;
+use led::LedRow;
+use lsm303agr::Lsm303agr;
 use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+use sensors::DataStore;
 
 static CHANNEL: Channel<ThreadModeRawMutex, ButtonDirection, 1> = Channel::new();
 
+hal::bind_interrupts!(struct Irqs {
+    SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => twim::InterruptHandler<hal::peripherals::TWISPI0>;
+});
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    rtt_init_print!();
     let p = embassy_nrf::init(Default::default());
 
     spawner
@@ -23,12 +37,79 @@ async fn main(spawner: Spawner) {
         .spawn(button_task(p.P0_23.degrade(), ButtonDirection::Right))
         .unwrap();
 
-    // Toggle LED
-    let mut led = led_pin(p.P0_28.degrade());
+    let _row1 = led_pin(p.P0_21.degrade());
+    let col = [
+        led_pin(p.P0_28.degrade()),
+        led_pin(p.P0_11.degrade()),
+        led_pin(p.P0_31.degrade()),
+        led_pin(p.P1_05.degrade()),
+        led_pin(p.P0_30.degrade()),
+    ];
 
-    loop{
-        led.toggle();
-        _ = Timer::after_millis(500);
+    let config = twim::Config::default();
+    let twim0 = Twim::new(p.TWISPI0, Irqs, p.P0_16, p.P0_08, config);
+
+    // Get sensor
+    let mut sensor = Lsm303agr::new_with_i2c(twim0);
+    let id = sensor.magnetometer_id().await.unwrap();
+    rprintln!("{:#02x?}", id);
+
+    // Initialise Sensor
+    sensor.init().await.unwrap();
+    sensor
+        .set_mag_mode_and_odr(
+            &mut Delay,
+            lsm303agr::MagMode::HighResolution,
+            lsm303agr::MagOutputDataRate::Hz50,
+        )
+        .await
+        .unwrap();
+
+    sensor
+        .set_accel_mode_and_odr(
+            &mut Delay,
+            lsm303agr::AccelMode::HighResolution,
+            lsm303agr::AccelOutputDataRate::Hz50,
+        )
+        .await
+        .unwrap();
+
+    let Ok(mut sensor) = sensor.into_mag_continuous().await else {
+        panic!("Error enabling continuous mode")
+    };
+
+    let mut datastore = DataStore::new(8);
+
+    if sensor.mag_status().await.unwrap().xyz_new_data() {
+        let magdata = sensor.magnetic_field().await.unwrap();
+        datastore.add_magnetic(magdata);
+    }
+    if sensor.accel_status().await.unwrap().xyz_new_data() {
+        let accdata = sensor.acceleration().await.unwrap();
+        datastore.add_accel(accdata);
+    }
+
+    // LED task:
+    let mut blinker = LedRow::new(col);
+    loop {
+        blinker.toggle();
+        if sensor.mag_status().await.unwrap().xyz_new_data() {
+            let magdata = sensor.magnetic_field().await.unwrap();
+            datastore.add_magnetic(magdata);
+        }
+        if sensor.accel_status().await.unwrap().xyz_new_data() {
+            let accdata = sensor.acceleration().await.unwrap();
+            datastore.add_accel(accdata);
+
+        }
+        select_biased! {
+            direction = CHANNEL.receive().fuse() => {
+                blinker.shift(direction);
+            }
+            _ = Timer::after_millis(500).fuse() => {
+                rprintln!("{:?}", datastore.get_averages());
+            }
+        }
     }
 }
 
---
+++ src/sensors.rs
@@ -0,0 +1,95 @@
+use heapless::Vec;
+use lsm303agr::{Acceleration, MagneticField};
+
+const MAX_SIZE: usize = 64;
+
+pub struct DataStore {
+    num_aves: u8,
+    acc: Vec<[i32; 3], MAX_SIZE>,
+    mag: Vec<[i32; 3], MAX_SIZE>,
+    temp: Vec<f64, MAX_SIZE>,
+}
+
+impl DataStore {
+    pub fn new(size: u8) -> DataStore {
+        DataStore {
+            num_aves: size,
+            acc: Vec::new(),
+            mag: Vec::new(),
+            temp: Vec::new()
+        }
+    }
+
+    fn add_vecdata(
+        &self,
+        data: [i32; 3],
+        mut vect: Vec<[i32; 3], MAX_SIZE>
+    ) ->  Vec<[i32; 3], MAX_SIZE> {
+        // Check to see whether vector is too long
+        if vect.len() as u8 >= self.num_aves {
+            vect.remove(0);
+        }
+        // Add data to vector
+        vect.push(data).unwrap();
+        vect
+    }
+
+    pub fn add_magnetic(&mut self, magnet_data: MagneticField) {
+        let data = [magnet_data.x_nt(),magnet_data.y_nt(), magnet_data.z_nt()];
+        self.mag = self.add_vecdata(data, self.mag.clone());
+    }
+
+    pub fn add_accel(&mut self, accel_data: Acceleration) {
+        let data = [accel_data.x_mg(), accel_data.y_mg(), accel_data.z_mg()];
+        self.acc = self.add_vecdata(data, self.acc.clone());
+    }
+
+    pub fn add_temp(&mut self, temp_data: f64) {
+        // Check to see whether we will exceed number of averages
+        if self.temp.len() as u8 >= self.num_aves {
+            self.temp.remove(0);
+        }
+
+        self.temp.push(temp_data).unwrap();
+    }
+
+    pub fn update_size(&mut self, new_size: u8) {
+        self.num_aves = new_size;
+    }
+
+    pub fn get_num_aves(&self) -> u8 {
+        self.num_aves
+    }
+
+    pub fn get_averages(&self) -> [f64; 7] {
+        // Get value sums
+        let accel_sum = sum_vecs(&self.acc);
+        let mag_sum = sum_vecs(&self.mag);
+        let temp_sum: f64 = self.temp.iter().sum();
+
+        // How long are vects
+        let len_acc = self.acc.len() as f64;
+        let len_mag = self.mag.len() as f64;
+        let len_temp = self.temp.len() as f64;
+
+        // Create array for data
+        let mut aves = [0.; 7];
+        for n in 0..3 {
+            aves[n] = accel_sum[n] as f64 / len_acc;
+            aves[n+3] = mag_sum[n] as f64 / len_mag;
+        }
+        aves[6] = temp_sum / len_temp;
+        aves
+    }
+}
+
+fn sum_vecs(vect: &Vec<[i32; 3], MAX_SIZE>) -> [i32; 3] {
+    //
+    let mut sum = [0, 0, 0];
+    for meas in vect.iter() {
+        for (n, val) in meas.iter().enumerate() {
+            sum[n] += val;
+        }
+    }
+    sum
+}
