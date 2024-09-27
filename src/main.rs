1f44950b2655619ef6ab26ec312eb484c9f4a9b2
Sam Hill <s.hill@yorksj.ac.uk>
2024-09-27 16:49:12 +0100

    Creates async function to read data and push to datastore

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod sensors;
 
 use button::ButtonDirection;
 use embassy_executor::Spawner;
-use embassy_nrf::{self as hal, gpio::{AnyPin, Input, Level, Output, OutputDrive, Pin, Pull}, twim::Twim, temp};
+use embassy_nrf::{self as hal, gpio::{AnyPin, Input, Level, Output, OutputDrive, Pin, Pull}, peripherals::TWISPI0, temp, twim::Twim};
 use embassy_nrf::temp::Temp;
 use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, channel::Channel};
 use embassy_time::Timer;
@@ -15,17 +15,19 @@ use embassy_time::Delay;
 use futures::{select_biased, FutureExt};
 use hal::twim;
 use led::LedRow;
-use lsm303agr::Lsm303agr;
+use lsm303agr::{interface::I2cInterface, mode::MagContinuous, Lsm303agr};
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 use sensors::DataStore;
 
 static CHANNEL: Channel<ThreadModeRawMutex, ButtonDirection, 1> = Channel::new();
+type SensorType<'a> = Lsm303agr<I2cInterface<Twim<'a, TWISPI0>>, MagContinuous>;
 
 hal::bind_interrupts!(struct Irqs {
     SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => twim::InterruptHandler<hal::peripherals::TWISPI0>;
     TEMP => temp::InterruptHandler;
 });
+
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -84,14 +86,8 @@ async fn main(spawner: Spawner) {
 
     let mut datastore = DataStore::new(8);
 
-    if sensor.mag_status().await.unwrap().xyz_new_data() {
-        let magdata = sensor.magnetic_field().await.unwrap();
-        datastore.add_magnetic(magdata);
-    }
-    if sensor.accel_status().await.unwrap().xyz_new_data() {
-        let accdata = sensor.acceleration().await.unwrap();
-        datastore.add_accel(accdata);
-    }
+    read_data(&mut datastore, &mut sensor).await;
+
     let temp_meas = temp.read().await.to_num::<f64>();
     datastore.add_temp(temp_meas);
 
@@ -99,18 +95,13 @@ async fn main(spawner: Spawner) {
     let mut blinker = LedRow::new(col);
     loop {
         blinker.toggle();
-        if sensor.mag_status().await.unwrap().xyz_new_data() {
-            let magdata = sensor.magnetic_field().await.unwrap();
-            datastore.add_magnetic(magdata);
-        }
-        if sensor.accel_status().await.unwrap().xyz_new_data() {
-            let accdata = sensor.acceleration().await.unwrap();
-            datastore.add_accel(accdata);
-        }
 
+        // Read data
+        read_data(&mut datastore, &mut sensor).await;
         let temp_meas = temp.read().await.to_num::<f64>();
         datastore.add_temp(temp_meas);
 
+        // Pick up button press or wait
         select_biased! {
             direction = CHANNEL.receive().fuse() => {
                 blinker.shift(direction);
@@ -138,5 +129,19 @@ async fn button_task(
         CHANNEL.send(direction).await;
         Timer::after_millis(100).await;
         input.wait_for_high().await;
+    }
+}
+
+async fn read_data<'a>(
+    data: &mut DataStore,
+    sensors: &mut SensorType<'a>
+) {
+    if sensors.mag_status().await.unwrap().xyz_new_data() {
+        let magdata = sensors.magnetic_field().await.unwrap();
+        data.add_magnetic(magdata);
+    }
+    if sensors.accel_status().await.unwrap().xyz_new_data() {
+        let accdata = sensors.acceleration().await.unwrap();
+        data.add_accel(accdata);
     }
 }
